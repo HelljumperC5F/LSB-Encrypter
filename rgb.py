6b78ccc47aa7f69c29e800ca071c8c7e33a68e52
Helljumper <162385498+HelljumperC5F@users.noreply.github.com>
2024-11-27 15:38:40 -0600

    feat: add rgb to rgb encoder

---
+++ rgb.py
@@ -0,0 +1,195 @@
+import streamlit as st
+import numpy as np
+import skimage
+import io
+
+
+def mse(imageA, imageB):
+    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+    err /= float(imageA.shape[0] * imageA.shape[1])
+    return err
+
+
+def psnr(imageA, imageB):
+    mse_value = mse(imageA, imageB)
+    if mse_value == 0:
+        return float('inf')
+    max_pixel = 255.0
+    return 20 * np.log10(max_pixel / np.sqrt(mse_value))
+
+
+def clicked_hide():
+    try:
+        cover_bytes = uploader_cover.read()
+    except AttributeError:
+        st.error('You did not upload a cover image.')
+        return
+
+    try:
+        secret_bytes = uploader_secret.read()
+    except AttributeError:
+        st.error('You did not upload a secret image.')
+        return
+
+    cover_image = skimage.io.imread(io.BytesIO(cover_bytes))
+    shape = [cover_image.shape[0], cover_image.shape[1]]
+
+    secret_image = skimage.io.imread(io.BytesIO(secret_bytes))
+    secret_image = skimage.transform.resize(secret_image, shape)
+    secret_image = secret_image * 255
+    secret_image = np.floor(secret_image)
+    secret_image = np.array(secret_image, int)
+
+    match bit_slider:
+        case 1:
+            secret_r = (secret_image[:, :, 0] >> 7) & 0b00000001
+            secret_g = (secret_image[:, :, 1] >> 7) & 0b00000001
+            secret_b = (secret_image[:, :, 2] >> 7) & 0b00000001
+
+            cover_image[:, :, 0] &= 0b11111110
+            cover_image[:, :, 1] &= 0b11111110
+            cover_image[:, :, 2] &= 0b11111110
+        case 2:
+            secret_r = (secret_image[:, :, 0] >> 6) & 0b00000011
+            secret_g = (secret_image[:, :, 1] >> 6) & 0b00000011
+            secret_b = (secret_image[:, :, 2] >> 6) & 0b00000011
+
+            cover_image[:, :, 0] &= 0b11111100
+            cover_image[:, :, 1] &= 0b11111100
+            cover_image[:, :, 2] &= 0b11111100
+        case 3:
+            secret_r = (secret_image[:, :, 0] >> 5) & 0b00000111
+            secret_g = (secret_image[:, :, 1] >> 5) & 0b00000111
+            secret_b = (secret_image[:, :, 2] >> 5) & 0b00000111
+
+            cover_image[:, :, 0] &= 0b11111000
+            cover_image[:, :, 1] &= 0b11111000
+            cover_image[:, :, 2] &= 0b11111000
+        case 4:
+            secret_r = (secret_image[:, :, 0] >> 4) & 0b00001111
+            secret_g = (secret_image[:, :, 1] >> 4) & 0b00001111
+            secret_b = (secret_image[:, :, 2] >> 4) & 0b00001111
+
+            cover_image[:, :, 0] &= 0b11110000
+            cover_image[:, :, 1] &= 0b11110000
+            cover_image[:, :, 2] &= 0b11110000
+        case 5:
+            secret_r = (secret_image[:, :, 0] >> 3) & 0b00011111
+            secret_g = (secret_image[:, :, 1] >> 3) & 0b00011111
+            secret_b = (secret_image[:, :, 2] >> 3) & 0b00011111
+
+            cover_image[:, :, 0] &= 0b11100000
+            cover_image[:, :, 1] &= 0b11100000
+            cover_image[:, :, 2] &= 0b11100000
+        case 6:
+            secret_r = (secret_image[:, :, 0] >> 2) & 0b00111111
+            secret_g = (secret_image[:, :, 1] >> 2) & 0b00111111
+            secret_b = (secret_image[:, :, 2] >> 2) & 0b00111111
+
+            cover_image[:, :, 0] &= 0b11000000
+            cover_image[:, :, 1] &= 0b11000000
+            cover_image[:, :, 2] &= 0b11000000
+        case 7:
+            secret_r = (secret_image[:, :, 0] >> 1) & 0b01111111
+            secret_g = (secret_image[:, :, 1] >> 1) & 0b01111111
+            secret_b = (secret_image[:, :, 2] >> 1) & 0b01111111
+
+            cover_image[:, :, 0] &= 0b10000000
+            cover_image[:, :, 1] &= 0b10000000
+            cover_image[:, :, 2] &= 0b10000000
+        case 8:
+            secret_r = secret_image[:, :, 0] & 0b11111111
+            secret_g = secret_image[:, :, 1] & 0b11111111
+            secret_b = secret_image[:, :, 2] & 0b11111111
+
+            cover_image[:, :, 0] &= 0b00000000
+            cover_image[:, :, 1] &= 0b00000000
+            cover_image[:, :, 2] &= 0b00000000
+
+    # st.write(format(secret_image[0][0], '08b'))
+    #
+    # st.write(format(secret_r[0][0], '08b'))
+    # st.write(format(secret_g[0][0], '08b'))
+    # st.write(format(secret_b[0][0], '08b'))
+
+    # cover_image = cover_image & 0b11111100
+
+    cover_image[:, :, 0] = cover_image[:, :, 0] | secret_r
+    cover_image[:, :, 1] = cover_image[:, :, 1] | secret_g
+    cover_image[:, :, 2] = cover_image[:, :, 2] | secret_b
+
+    decoded_image = np.empty(cover_image.shape, int)
+    # decoded_image = 0b00000000
+
+    match bit_slider:
+        case 1:
+            decoded_r = (cover_image[:, :, 0] & 0b00000001) << 7
+            decoded_g = (cover_image[:, :, 1] & 0b00000001) << 7
+            decoded_b = (cover_image[:, :, 2] & 0b00000001) << 7
+        case 2:
+            decoded_r = (cover_image[:, :, 0] & 0b00000011) << 6
+            decoded_g = (cover_image[:, :, 1] & 0b00000011) << 6
+            decoded_b = (cover_image[:, :, 2] & 0b00000011) << 6
+        case 3:
+            decoded_r = (cover_image[:, :, 0] & 0b00000111) << 5
+            decoded_g = (cover_image[:, :, 1] & 0b00000111) << 5
+            decoded_b = (cover_image[:, :, 2] & 0b00000111) << 5
+        case 4:
+            decoded_r = (cover_image[:, :, 0] & 0b00001111) << 4
+            decoded_g = (cover_image[:, :, 1] & 0b00001111) << 4
+            decoded_b = (cover_image[:, :, 2] & 0b00001111) << 4
+        case 5:
+            decoded_r = (cover_image[:, :, 0] & 0b00011111) << 3
+            decoded_g = (cover_image[:, :, 1] & 0b00011111) << 3
+            decoded_b = (cover_image[:, :, 2] & 0b00011111) << 3
+        case 6:
+            decoded_r = (cover_image[:, :, 0] & 0b00111111) << 2
+            decoded_g = (cover_image[:, :, 1] & 0b00111111) << 2
+            decoded_b = (cover_image[:, :, 2] & 0b00111111) << 2
+        case 7:
+            decoded_r = (cover_image[:, :, 0] & 0b01111111) << 1
+            decoded_g = (cover_image[:, :, 1] & 0b01111111) << 1
+            decoded_b = (cover_image[:, :, 2] & 0b01111111) << 1
+        case 8:
+            decoded_r = cover_image[:, :, 0] & 0b11111111
+            decoded_g = cover_image[:, :, 1] & 0b11111111
+            decoded_b = cover_image[:, :, 2] & 0b11111111
+
+    decoded_image[:, :, 0] = decoded_r
+    decoded_image[:, :, 1] = decoded_g
+    decoded_image[:, :, 2] = decoded_b
+
+    st.success('Image Encrypted!')
+    st.subheader('Metricas de error')
+    st.text(f'MSE: {mse(secret_image, decoded_image)}')
+    st.text(f'PSNR: {psnr(secret_image, decoded_image)}')
+    st.header('Encrypted Image')
+    st.image(cover_image)
+    st.header('Decrypted Secret Image')
+    st.image(decoded_image)
+
+
+st.title('LSB Steganography Encrypter')
+
+bit_slider = st.slider(
+        label='Number of bits to use on the cover image:',
+        min_value=1,
+        max_value=8,
+        value=6
+        )
+
+uploader_cover = st.file_uploader(
+        label='Cover Image (The image you want to show)',
+        type=['jpg', 'jpeg']
+        )
+
+uploader_secret = st.file_uploader(
+        label='Secret Image (The image you want to hide)',
+        type=['jpg', 'jpeg']
+        )
+
+st.button(
+        label='Hide',
+        type='primary',
+        use_container_width=True,
+        on_click=clicked_hide)
